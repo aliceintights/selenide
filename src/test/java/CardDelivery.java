caef551df4b153b29e9fca12afca171803c103d4
irina <obesyuk@gmail.com>
2024-04-16 20:36:12 +0300

    tests adjusted

--- src/test/java/CardDelivery.java
+++ src/test/java/CardDelivery.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Test;
@@ -20,29 +21,33 @@ class CardDelivery {
     @Test
     void cardOrderMainTest() {
         open("http://localhost:9999");
+        var deliveryDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         $("[placeholder='Город']").setValue("Санкт-Петербург");
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        $("[placeholder='Дата встречи']").setValue(deliveryDate);
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79007007009");
         $(".checkbox__box").click();
         $(".button").click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("04.04.2024"));
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Успешно! Встреча успешно забронирована на " + deliveryDate));
     }
 
     @Test
     void cardOrderSecondTest() {
         open("http://localhost:9999");
+        var deliveryDate = LocalDate.now().plusDays(21).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        var deliveryMonth = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         $("[placeholder='Город']").type("Пе");
         $(byText("Петропавловск-Камчатский")).click();
         $("[placeholder='Дата встречи']").click();
-        $$(".calendar__day").findBy(text("8")).click();
+        $(".calendar__arrow.calendar__arrow_direction_right").click();
+        $$(".calendar__day").findBy(text("7")).click();
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79007007009");
         $(".checkbox__box").click();
         $(".button").click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("08.04.2024"));
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Успешно! Встреча успешно забронирована на " + deliveryDate));
     }
 }
